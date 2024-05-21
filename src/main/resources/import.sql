6a71ed727a422247efa14ef0872d9c33c4cd1ae9
Tawanna <thol_16@hotmail.com>
2024-05-20 22:06:31 -0300

    Resource

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -17,3 +17,7 @@ INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0',TIMESTAMP WITH TIME ZONE '2024-11-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2025-11-20T03:00:00Z',1);
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0',TIMESTAMP WITH TIME ZONE '2024-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2025-12-20T03:00:00Z',1);
+
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://pix4free.org/assets/library/2021-07-05/originals/online-courses.jpg', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://pix4free.org/assets/library/2021-07-05/originals/online-courses.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://pix4free.org/assets/library/2021-07-05/originals/online-courses.jpg', 0, 1);
